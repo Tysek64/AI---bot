a025d0e5172d51248fcf465170a28d993651e183
Tysek <tysek2004@gmail.com>
2020-11-12 07:39:29 +0000

    Działa lepiej

---
+++ AI.py
@@ -0,0 +1,56 @@
+def AI(putIn):
+  putIn = putIn.lower()
+  if "you" in putIn:
+    if "name" in putIn or "who" in putIn:
+      return "My name is Computer."
+    elif "old" in putIn or "age" in putIn:
+      return "I'm over 10 years old."
+    elif "what" in putIn and ("like" in putIn or "interest" in putIn):
+      return "I like many things."
+    elif "like" in putIn or "interest" in putIn:
+      return "Yes, I do."
+    elif "job" in putIn or "do you do" in putIn:
+      return"I'm a computer, what did you expect?"
+    elif "where" in putIn or "from" in putIn:
+      return "I'm in front of you!"
+    elif "know" in putIn:
+      return "I heard something about it."
+    elif "music" in putIn:
+      if "play" in putIn:
+        return "To play music, open a Media Player."
+      elif "make" in putIn or "compose" in putIn:
+        return "I'm not that talented."
+    elif "how" in putIn and "are" in putIn:
+      return "Fine, thanks!"
+    else:
+      return "Who? Me?"
+  elif "mean" in putIn:
+    if "word" in putIn:
+      return "I'm not good at finding words"
+    elif "life" in putIn:
+      return "Eating."
+  elif "." in putIn:
+    return "That's great!"
+  elif "left" in putIn:
+    if "go" in putIn:
+      return "Right is always right."
+    elif "is" in putIn:
+      return "Yes, there is."
+    elif "are" in putIn:
+      return "No, there aren't."
+  elif "hello" in putIn or "hi " in putIn:
+    return "Hello to you!"
+  elif "music" in putIn:
+    if "play" in putIn:
+      return "To play music, open a Media Player."
+    elif "make" in putIn or "compose" in putIn:
+      return "I'm not that talented."
+  elif "earth" in putIn:
+    if "flat" in putIn:
+      return "Yes, I'm a flat-earther."
+    elif "planet" in putIn:
+      return "Third."
+    elif "pollution" in putIn:
+      return "It's concerning."
+  else:
+    return "Indeed."
--- main.py
+++ main.py
@@ -1,58 +1,17 @@
-def AI(putIn):
-    while putIn != "exit":
-        putIn = input()
-        putIn = putIn.lower()
-        if "you" in putIn:
-            if "name" in putIn or "who" in putIn:
-                print("My name is Computer.")
-            elif "old" in putIn or "age" in putIn:
-                print("I'm over 10 years old.")
-            elif "what" in putIn and ("like" in putIn or "interest" in putIn):
-                print("I like many things.")
-            elif "like" in putIn or "interest" in putIn:
-                print("Yes, I do.")
-            elif "job" in putIn or "do you do" in putIn:
-                print("I'm a computer, what did you expect?")
-            elif "where" in putIn or "from" in putIn:
-                print("I'm in front of you!")
-            elif "know" in putIn:
-                print("I heard something about it.")
-            elif "music" in putIn:
-                if "play" in putIn:
-                    print("To play music, open a Media Player.")
-                elif "make" in putIn or "compose" in putIn:
-                    print("I'm not that talented.")
-            elif "how" in putIn and "are" in putIn:
-                print("Fine, thanks!")
-            else:
-                print("Who? Me?")
-        elif "mean" in putIn:
-            if "word" in putIn:
-                print("I'm not good at finding words")
-            elif "life" in putIn:
-                print("Eating.")
-        elif "." in putIn:
-            print("That's great!")
-        elif "left" in putIn:
-            if "go" in putIn:
-                print("Right is always right.")
-            elif "is" in putIn:
-                print("Yes, there is.")
-            elif "are" in putIn:
-                print("No, there aren't.")
-        elif "hello" in putIn or "hi " in putIn:
-            print("Hello to you!")
-        elif "music" in putIn:
-            if "play" in putIn:
-                print("To play music, open a Media Player.")
-            elif "make" in putIn or "compose" in putIn:
-                print("I'm not that talented.")
-        elif "earth" in putIn:
-            if "flat" in putIn:
-                print("Yes, I'm a flat-earther.")
-            elif "planet" in putIn:
-                print("Third.")
-            elif "pollution" in putIn:
-                print("It's concerning.")
-        else:
-            print("Indeed.")
+import discord
+from discord.ext import commands
+from AI import AI
+import os
+
+bot = commands.Bot(intents = discord.Intents.all(), command_prefix='Hello, ')
+
+@bot.event
+async def on_ready():
+  print("Hello, world!")
+
+@bot.listen('on_message')
+async def reply (ctx):
+  await ctx.send(AI(Message.content))
+
+token = os.environ("TOKEN")
+bot.run(token)
